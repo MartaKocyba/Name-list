4c84433e74a3b7f11cb26e1507d836013f5fc83c
MartaKocyba <116761391+MartaKocyba@users.noreply.github.com>
2022-12-14 23:07:06 +0100

    Add files via upload

---
+++ listofnames.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText("./../../../lista imion.txt");
+            string[] arrtext = text.Split("\n");
+            foreach (var name in arrtext)
+            {
+                if (name.Contains("Anna "))
+                {
+                    Console.WriteLine(name);
+                }
+            }
+            Console.WriteLine("==========");
+            Console.WriteLine("246 imię i nazwisko to: " + arrtext[246]);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
